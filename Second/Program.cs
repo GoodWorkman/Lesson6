404979e0be523e3f08c9fd15a89f8953c88b333a
Nikaragis <nikolaj@iMac-Nikolaj.local>
2022-01-12 19:50:42 +0300

    Homework from lesson 6 2 tasks

---
+++ Second/Program.cs
@@ -0,0 +1,67 @@
+﻿
+/*
+ * Модифицировать программу нахождения минимума функции так, чтобы можно было передавать функцию в виде делегата. 
+а) Сделать меню с различными функциями и представить пользователю выбор, для какой функции и на каком отрезке находить минимум. 
+Использовать массив (или список) делегатов, в котором хранятся различные функции.
+
+
+ * Anton Manakov
+ */
+using System;
+using System.IO;
+
+namespace Second
+
+{
+    class Program
+    {
+        public delegate double function(double x);
+        public static double F1(double x)
+        {
+            return x * x - 50 * x + 10;
+        }
+
+        public static double F2(double x)
+        {
+            return x * x - 10 * x + 50;
+        }
+        public static void SaveFunc(string fileName, double a, double b, double h, function F)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(F(x));
+                x += h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+        public static double Load(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            double min = double.MaxValue;
+            double d;
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                
+                d = bw.ReadDouble();
+                if (d < min) min = d;
+            }
+            bw.Close();
+            fs.Close();
+            return min;
+        }
+        static void Main(string[] args)
+        {
+            function[] F = { F1, F2 };
+            Console.WriteLine("Сделайте выбор: 1 - функция 1, 2 - функция 2");
+            int index = int.Parse(Console.ReadLine());
+            SaveFunc("data.bin", -100, 100, 0.5, F[index - 1]);
+            Console.WriteLine(Load("data.bin"));
+            Console.ReadKey();
+        }
+    }
+}
